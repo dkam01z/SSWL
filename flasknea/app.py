f577d5d97269be988b607317b3c0bf08a161ec3a
dkam01z <130308987+dkam01z@users.noreply.github.com>
2024-02-25 02:08:24 +0000

    removed Unnecessary imports

--- flasknea/app.py
+++ flasknea/app.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from flask.sessions import NullSession
 from wtforms.fields.core import BooleanField, DateField, DecimalField, FloatField, IntegerField, SelectField, SelectMultipleField, TimeField
-from data import Events
+
 from flask import Flask , render_template, flash, redirect, url_for , session , logging, request
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField,PasswordField, validators, SelectField
